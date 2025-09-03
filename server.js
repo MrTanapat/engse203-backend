5d28be35c057075698ac1af9c61d01bc2b385fc0
SE-Tanapat <tan.tanapat2002@gmail.com>
2025-09-03 11:04:46 +0700

    feat Install libralies #cors

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME;
 
-// สร้าง Route (เส้นทาง) แรกสำหรับหน้าหลัก
+const cors = require("cors"); // Import cors
+app.use(cors());
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "This data is open for everyone!" });
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>Hello from ENGSE203 Express Server!</h1>Hi Express");
 });
 
-// เริ่มต้นให้ Server รอรับการเชื่อมต่อ
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
